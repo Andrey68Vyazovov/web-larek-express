cbf8eb0067867c29246ee14b9fa8ba628ec15aaf
Andrey Vyazovov <andreyy-vjazovov@rambler.ru>
2024-11-08 16:12:37 +0300

    feat: add express server with mongo connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,2 +1,31 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import path from 'path';
+import { errors } from 'celebrate';
+
+import routers from './routes';
+import { errorLogger, requestLogger } from './middlewares/logger';
+
+const { PORT = 3000, DB_ADDRESS = 'mongodb://localhost:27017/weblarek' } = process.env;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(requestLogger);
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(DB_ADDRESS);
+
+
+app.use('/', routers);
+
+app.use(errorLogger);
+app.use(errors());
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
